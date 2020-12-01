a0c739f46874bca297d1c568f4c48c7bf5a43e7f
José Henrique Ivanchechen <jose.henrique.ivan@gmail.com>
2020-12-01 12:53:16 -0300

    Adicionar arquivos de projeto.

---
+++ frmMain.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using ZXing;
+using ZXing.Common;
+using ZXing.QrCode;
+
+namespace QReader
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+            qrCodeImage.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void readClipboard_Click(object sender, EventArgs e)
+        {
+            QRCodeReader reader = new QRCodeReader();
+            textResult.ResetText();
+            linkResult.ResetText();
+
+            Bitmap imageCode;
+            try
+            {
+                Image image = Clipboard.GetImage();
+                imageCode = new Bitmap(image);
+                qrCodeImage.Image = image;
+            }
+            catch (NullReferenceException)
+            {
+                textResult.Text = "Clipboard doesn't contain an image!";
+                linkResult.Text = "Clipboard doesn't contain an image!";
+                return;
+            }
+
+            BitmapLuminanceSource bls = new BitmapLuminanceSource(imageCode);
+            GlobalHistogramBinarizer binarizer = new GlobalHistogramBinarizer(bls);
+            BinaryBitmap bb = new BinaryBitmap(binarizer);
+
+            Result result = reader.decode(bb);
+
+            if (result == null)
+            {
+                textResult.Text = "result is null!";
+                linkResult.Text = "result is null!";
+                return;
+            }
+
+            textResult.Text = result.Text;
+            linkResult.Text = result.Text;
+
+            if (autoOpen.Checked &&
+                (result.Text.StartsWith("http://") ||
+                result.Text.StartsWith("https://")) )
+            {
+                System.Diagnostics.Process.Start(linkResult.Text);
+            }
+
+            if (autoCopy.Checked)
+            {
+                Clipboard.SetText(result.Text);
+            }
+        }
+    }
+}
